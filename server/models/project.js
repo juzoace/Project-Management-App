a6918b95fb96a32629af58a27d65e05625a4779b
Nwigwe Uzochukwu <uzochukwunwigwe@gmail.com>
2021-04-27 18:23:19 +0100

    Fixed issue with delete project operation Project Reference on User model needs to be deleted whenever a project is deleted

--- server/models/project.js
+++ server/models/project.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+
+
 const ProjectSchema = new Schema({
     title: {type: String, required: true},
     description: {type: String, required: true},
@@ -17,5 +19,11 @@ const ProjectSchema = new Schema({
         default: 'Active'
     }
 });
+
+function removeLinkedDocuments() {
+    Event.remove({_id: { $in: doc.owner}})
+}
+ProjectSchema.post('remove', removeLinkedDocuments); 
+    
 
 mongoose.model('Project', ProjectSchema);
--- server/routes/project.js
+++ server/routes/project.js
@@ -1,104 +1,122 @@
 const router = require('express').Router();
 const mongoose = require('mongoose');
-// const Project = mongoose.model('Project');
 const passport = require('passport');
 const utils = require('../lib/utils');
 const Project = mongoose.model('Project');
 const User = mongoose.model('User');
-const jsonwebtoken = require('jsonwebtoken');
-
 
 router.get('/fetchProjects', passport.authenticate('jwt', {session: false}), async(req, res, next ) => {
-    console.log(req.headers.authorization)
-    // console.log(req.body);
+    
+    // Get the user Id
     await User.findById(req.body._id).populate("projects")
     .then((project) => {
-        console.log(project.projects)
+       
+         // Send response to client
         res.status(200).json({success: true, data: project.projects})
     })
     .catch((err) => {
+
+         // Send response to client
         res.status(401).json({success: false, msg: err})
     })
-    // console.log(project.projects);
-   
-
-
     
-
 })
 
 router.post('/createProject', passport.authenticate('jwt', {session: false}), async(req, res, next ) => {
-    console.log(req.body);
-    let userId = await User.findById(req.body._id)
-    // console.log(projects
-    // .then((user) => {
 
-    //     console.log(user)
-    //     // foundUser = user
-    // })
+    // Get the user Id
+    await User.findById(req.body._id)
+    .then((userId) => {
+     
+        if (userId._id !== null) {
+            const newProject = new Project({
+                title: req.body.title,
+                description: req.body.description,
+                deadline: req.body.description,
+                budget: req.body.budget,
+                owner: userId._id
+            })
+    
 
-    console.log(userId._id);
-    console.log(req.body.title)
-    if (userId !== undefined ) {
-        const newProject = new Project({
-            title: req.body.title,
-            description: req.body.description,
-            deadline: req.body.description,
-            budget: req.body.budget,
-            owner: userId._id
-        })
+            newProject.save()
+            .then((project) => {
 
+                 // Send response to client
+                res.status(200).json({success: true, data: project})
+        
+            })
+            .catch((err) => {
 
-        await newProject.save()
-        console.log(userId)
-        userId.projects.push(newProject);
-        await userId.save();
-        // .then((project) => {
-        //     console.log(project)
-        // })
-        // .catch((err) => {
-        //     console.log(err)
-        // })
-    }
+                 // Send response to client
+                res.status(401).json({success: false, msg: err})
+            })
 
-
-    // console.log(foundUser)
-
+            // must work push new project to the user
+            userId.projects.push(newProject);
+    
+            userId.save()
+        }
+       
+        
+    })
+    .catch((err) => {
+         // Send response to client
+        res.status(401).json({success: false, msg: "Something went wrong"})
+    })
+  
 });
     
 router.post('/updateProject', passport.authenticate('jwt', {session: false}), async(req, res, next) => {
-    
 
     // get particular project _id
-    console.log(req.body)
     await Project.findOneAndUpdate( {_id: req.body._id}, {
+
         title : req.body.title,
         description : req.body.description,
         deadline : req.body.deadline,
         budget : req.body.budget,
         status : req.body.status
+
     }, { useFindAndModify: false })
     .then((project) => {
-        console.log(project);
+
+
+        // Send response to client
+        res.status(200).json({success: true, data: project.projects})
+
+    })
+    .catch((err) => {
+
+        // Send response to client
+        res.status(401).json({success: false, msg: "Something went wrong"})
+    
     })
 
-
-    // await Project.findById(req.body._id)
-    // .then(project => {
-        // project.title = req.body.title;
-        // project.description = req.body.description;
-        // project.deadline = req.body.deadline;
-        // project.budget = req.body.budget;
-        // project.status = req.body.status;
-    //     project.save();
-    // })
-    // .then(() => {
-    //     res.status().json({success: true})
-    // })
 })
 
-router.post('/delete', passport.authenticate('jwt', {session: false}), async(req, res, next) => {
+router.post('/deleteProject', passport.authenticate('jwt', {session: false}), async(req, res, next) => {
+    // get particular project _id
+    await Project.findByIdAndRemove({_id: req.body._id})
+    .then((project) => {
+        
+        if(project === null) {
+        // Send response to client
+        res.status(401).json({success: false, msg: "Something went wrong"})
     
+        }
+
+        // Send response to client
+        res.status(200).json({success: true})
+
+    })
+    .catch((err) => {
+   
+        // Send response to client
+        res.status(401).json({success: false, msg: err})
+    
+    })
+        
+
 })
 
 module.exports = router;
